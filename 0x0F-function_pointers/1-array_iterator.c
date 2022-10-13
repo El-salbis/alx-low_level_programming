a470b6ce9be2b934e4270a5030c289cd3864db33
sabiu ishiyaku <salbis2020@gmail.com>
2022-10-13 09:52:22 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,28 +1,19 @@
+#include <stdio.h>
 #include "function_pointers.h"
-/**
- * array_iterator - executes a functionas parameter of each element of an array
- *
- * @array: pointer to an int
- * @size: variable type size_t
- * @action: pointer to a function that receives an int
- *
- * Return: void
- */
-void array_iterator(int *array, size_t size, void (*action)(int))
+
+
+void print_elem(int elem)
 {
-	/* unsigned int because an error told me */
-	/* and because the function is printing integers */
-	/* and then, printing those integers in hexa */
-	/* so elements in the array unsigned integers */
-	unsigned int elements;
-	/* run through the array to know which are the elements */
-	/* we already know the size of the array */
-	/* elements runs while it is less than the size ofc. */
-	if (action && array)
-	{
-		for (elements = 0; elements < size; elements++)
-			action(array[elements]);
-	}
-	/* so de-reference the pointer to the function */
-	/* to the elements of the array */
+	printf("%d\n", elem);
+}
+
+void print_elem_hex(int elem)
+{
+	printf("0x%x\n", elem);
+}
+
+int main(void)
+{
+	int array[5] = {0, 98, 402, 1024, 4096};
+	return (0);
 }
